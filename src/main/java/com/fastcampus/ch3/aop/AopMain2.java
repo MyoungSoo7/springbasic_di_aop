749bd6305754fb88f9a6c41e54485aff7d169c3c
iamip <iamipro@naver.com>
2023-03-20 21:47:15 +0900

    aop , transactional

--- src/main/java/com/fastcampus/ch3/aop/AopMain2.java
+++ src/main/java/com/fastcampus/ch3/aop/AopMain2.java
@@ -5,11 +5,11 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class AopMain2 {
     public static void main(String[] args) {
-        ApplicationContext ac = new GenericXmlApplicationContext("file:src/ma\n" +
-                "    }in/webapp/WEB-INF/spring/**/root-context_aop.xml");
+        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context_aop.xml");
         MyMath mm = (MyMath) ac.getBean("myMath");
         mm.add(3, 5);
         mm.add(1, 2, 3);
         System.out.println("mm.multiply(3, 5) = " + mm.multiply(3, 5));
+
     }
 }
